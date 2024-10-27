929b8f2cee86ca925a92918e0c1bf85e3a2c3f42
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2024-10-28 03:41:44 +0500

    config changes

--- gmail-service/internal/pkg/config/getEnv.go
+++ gmail-service/internal/pkg/config/getEnv.go
@@ -16,15 +16,6 @@ type Config struct {
 	PostgresUser     string
 	PostgresPassword string
 	PostgresDatabase string
-	LogPath          string
-	KafkaUrl         string
-	MongoUrl         string
-	NotificationUrl  string
-	BucketName       string
-	MinioUrl         string
-	MinioUser        string
-	MinioPassword    string
-	MinioPath        string
 }
 
 func Load() Config {
@@ -41,15 +32,6 @@ func Load() Config {
 	config.PostgresUser = cast.ToString(getOrReturnDefaultValue("POSTGRES_USER", "pogres"))
 	config.PostgresPassword = cast.ToString(getOrReturnDefaultValue("POSTGRES_PASSWORD", "1234"))
 	config.PostgresDatabase = cast.ToString(getOrReturnDefaultValue("POSTGRES_DATABASE", "proycts"))
-	config.LogPath = cast.ToString(getOrReturnDefaultValue("LOG_PATH", "logs/info.log"))
-	config.KafkaUrl = cast.ToString(getOrReturnDefaultValue("KAFKA_URL", "q"))
-	config.MongoUrl = cast.ToString(getOrReturnDefaultValue("MONGO_URL", "q"))
-	config.NotificationUrl = cast.ToString(getOrReturnDefaultValue("NOTIFICATION_URL", "q"))
-	config.BucketName = cast.ToString(getOrReturnDefaultValue("BUCKET_NAME", "q"))
-	config.MinioUrl = cast.ToString(getOrReturnDefaultValue("MINIO_URL", "q"))
-	config.MinioUser = cast.ToString(getOrReturnDefaultValue("MINIO_USER", "q"))
-	config.MinioPassword = cast.ToString(getOrReturnDefaultValue("MINIO_PASSWORD", "q"))
-	config.MinioPath = cast.ToString(getOrReturnDefaultValue("MINIO_PATH", "q"))
 
 	return config
 }
